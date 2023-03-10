831a768df75ff01d9c2fe2ce27bed36cd2d059c1
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-10 20:03:40 +0530

    new images added

--- src/assets/index.js
+++ src/assets/index.js
@@ -27,9 +27,12 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import project1 from "./project1.png";
+import project2 from "./project2.png";
+import project3 from "./project3.png";
+import project4 from "./project4.png";
+import project5 from "./project5.png";
+import project6 from "./project6.png";
 
 export {
   logo,
@@ -58,7 +61,10 @@ export {
   shopify,
   starbucks,
   tesla,
-  carrent,
-  jobit,
-  tripguide,
+  project1,
+  project2,
+  project3,
+  project4,
+  project5,
+  project6,
 };
