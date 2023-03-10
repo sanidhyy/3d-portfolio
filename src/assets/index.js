d1aa612025af2f32336c6090beae2e48975f4e58
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-10 14:41:09 +0530

    preview image added

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,7 @@ import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
+import preview from "./preview.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
@@ -37,6 +38,7 @@ export {
   mobile,
   web,
   github,
+  preview,
   menu,
   close,
   css,
