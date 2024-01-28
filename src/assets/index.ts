64e21734599d54dd6958584798282700c53766ef
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-28 07:43:21 +0000

    assets reorder

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,15 +1,15 @@
 // Export new images here
 // Don't remove anything from here if not sure
 
-import logo from "./logo.svg";
 import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
-import preview from "./preview.png";
-import menu from "./menu.svg";
-import close from "./close.svg";
 import bulb from "./bulb.svg";
+import close from "./close.svg";
+import creator from "./creator.png";
+import logo from "./logo.svg";
+import menu from "./menu.svg";
+import mobile from "./mobile.png";
+import preview from "./preview.png";
+import web from "./web.png";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -22,40 +22,40 @@ import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import typescript from "./tech/typescript.png";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import project1 from "./project1.png";
-import project2 from "./project2.png";
-import project3 from "./project3.png";
-import project4 from "./project4.png";
-import project5 from "./project5.png";
-import project6 from "./project6.png";
+import project1 from "./projects/project1.png";
+import project2 from "./projects/project2.png";
+import project3 from "./projects/project3.png";
+import project4 from "./projects/project4.png";
+import project5 from "./projects/project5.png";
+import project6 from "./projects/project6.png";
 
 import user1 from "./testimonials/user1.jpg";
 import user2 from "./testimonials/user2.jpg";
 import user3 from "./testimonials/user3.jpg";
 
 import github from "./socials/github.svg";
-import youtube from "./socials/youtube.svg";
 import linkedin from "./socials/linkedin.svg";
 import twitter from "./socials/twitter.svg";
+import youtube from "./socials/youtube.svg";
 
 export {
-  logo,
   backend,
-  creator,
-  mobile,
-  web,
-  preview,
-  menu,
-  close,
   bulb,
+  close,
+  creator,
+  logo,
+  menu,
+  mobile,
+  preview,
+  web,
   css,
   docker,
   figma,
