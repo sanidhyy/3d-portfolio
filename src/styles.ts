0a2d04b0f96e5090783525ce27d5eb3ac434c6ff
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-28 09:25:28 +0000

    styles updated

--- src/styles.ts
+++ src/styles.ts
@@ -13,4 +13,4 @@ export const styles = {
     "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
   sectionSubText:
     "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
-};
+} as const;
