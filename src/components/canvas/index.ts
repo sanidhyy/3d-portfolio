c5434126aff5dd0207a0f8ee1d3d2b164ec6b604
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-28 08:31:37 +0000

    imports reorder

--- src/components/canvas/index.ts
+++ src/components/canvas/index.ts
@@ -1,9 +1,9 @@
 // Export 3d models here
 // don't remove anything if not sure
 
-import EarthCanvas from "./earth";
 import BallCanvas from "./ball";
 import ComputersCanvas from "./computers";
+import EarthCanvas from "./earth";
 import StarsCanvas from "./stars";
 
 export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
